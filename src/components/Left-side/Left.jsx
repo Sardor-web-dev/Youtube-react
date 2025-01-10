6ce718b0bec43a068344f26c1658d7a76b8b6c5b
SarikViper <dzamolovsardor5@gmail.com>
2025-01-11 00:16:33 +0500

    added icon and title

---
+++ src/components/Left-side/Left.jsx
@@ -0,0 +1,92 @@
+import './style.css'
+
+function Left() {
+    return ( 
+        <>
+            <main>
+      <div className='navigation'>
+        <div className='Home'>
+          <img src="" alt="" />
+          <p>Home</p>
+        </div>
+        <div className='Explore'>
+          <img src="" alt="" />
+          <p>Explore</p>
+        </div>
+        <div className='Shorts'>
+          <img src="" alt="" />
+          <p>Shorts</p>
+        </div>
+        <div className='Subscriptions'>
+          <img src="" alt="" />
+          <p>Subscriptions</p>
+        </div>
+
+        <hr />
+        <div className='Library'>
+          <img src="" alt="" />
+          <p>Library</p>
+        </div>
+        <div className='History'>
+          <img src="" alt="" />
+          <p>History</p>
+        </div>
+        <div className='Your videos'>
+          <img src="" alt="" />
+          <p>Your videos</p>
+        </div>
+        <div className='Watch later'>
+          <img src="" alt="" />
+          <p>Watch later</p>
+        </div>
+        <div className='Liked videos'>
+          <img src="" alt="" />
+          <p>Liked videos</p>
+        </div>
+        <div className='Show more'>
+          <img src="" alt="" />
+          <p>Show more</p>
+        </div>
+
+        <hr />
+        <p>Subscriptions</p> {/*скоро будет вместо этого настоящие подписки с API */}
+        <div className='channel1'>
+          <img src="" alt="" />
+           <p>Nadir one The go</p>  {/*скоро будет вместо этого настоящие подписки с API */}
+        </div>
+        <div className='channel1'>
+          <img src="" alt="" />
+           <p>Nadir one The go</p>  {/*скоро будет вместо этого настоящие подписки с API */}
+        </div>
+        <div className='channel1'>
+          <img src="" alt="" />
+           <p>Nadir one The go</p>  {/*скоро будет вместо этого настоящие подписки с API */}
+        </div>
+        <div className='channel1'>
+          <img src="" alt="" />
+           <p>Nadir one The go</p>  {/*скоро будет вместо этого настоящие подписки с API */}
+        </div>
+        <div className='channel1'>
+          <img src="" alt="" />
+           <p>Nadir one The go</p>  {/*скоро будет вместо этого настоящие подписки с API */}
+        </div>
+        <div className='channel1'>
+          <img src="" alt="" />
+           <p>Nadir one The go</p>  {/*скоро будет вместо этого настоящие подписки с API */}
+        </div>
+        <div className='channel1'>
+          <img src="" alt="" />
+           <p>Nadir one The go</p>  {/*скоро будет вместо этого настоящие подписки с API */}
+        </div>
+        <div className='channel1'>
+          <img src="" alt="" />
+           <p>Nadir one The go</p>  {/*скоро будет вместо этого настоящие подписки с API */}
+        </div>
+        
+      </div>
+    </main>
+        </>
+     );
+}
+
+export default Left;
